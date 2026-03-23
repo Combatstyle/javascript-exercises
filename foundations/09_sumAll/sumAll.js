4f16125bc22e6ddf76fcc11887d7e5a7329c2fd0
philip <undervath@gmail.com>
2026-03-23 19:00:26 +0700

    Add logic to the sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let min = Math.min(start, end);
+    let max = Math.max(start, end);
 
+    let n = (max - min + 1);
+
+    return (n * (min + max)) / 2;
 };
+
+sumAll(2, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
