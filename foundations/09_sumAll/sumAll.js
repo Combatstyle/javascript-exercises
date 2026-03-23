c1dc8c9d779f1989c2825b2f68b62f7c23a510e5
philip <undervath@gmail.com>
2026-03-23 19:08:06 +0700

    Add error handling to sumAll function

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,13 @@
 const sumAll = function(start, end) {
+
+    if (
+        typeof start !== 'number' || typeof end !== 'number' ||
+        !Number.isInteger(start) || !Number.isInteger(end) ||
+        start < 0 || end < 0
+    ) {
+        return "ERROR";
+    }
+
     let min = Math.min(start, end);
     let max = Math.max(start, end);
 
