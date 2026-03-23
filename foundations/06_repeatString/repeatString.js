0e9191d5d17e5bfb637c3742e470414b5b24fd20
philip <undervath@gmail.com>
2026-03-23 12:59:12 +0700

    create function to check num and iterate string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num <= 0) {
+        return "ERROR";
+    } else {
+        let result = '';
+        for (let i = 1; i <= num; i++) {
+            result += string;
+        }
+        return result;
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
