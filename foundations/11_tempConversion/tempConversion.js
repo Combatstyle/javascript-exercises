36e6ad8602edd06149cc9bc3dd316481b7bc8ee5
philip <undervath@gmail.com>
2026-03-24 14:35:05 +0700

    add logic to tempConversion functions, del .skip

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  return Math.floor(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5 + 32);
+  return Math.floor(fahrenheit * 10) / 10;
 };
+
+convertToCelsius(32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
