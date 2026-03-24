2cc170322b65cc76ea11df164fb58ce2eb17177f
philip <undervath@gmail.com>
2026-03-24 14:38:52 +0700

    change Math.floor to Math.round

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   let celsius = (fahrenheit - 32) * 5 / 9;
-  return Math.floor(celsius * 10) / 10;
+  return Math.round(celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
   let fahrenheit = (celsius * 9/5 + 32);
-  return Math.floor(fahrenheit * 10) / 10;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 convertToCelsius(32);
